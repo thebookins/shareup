894702c10371a149f7b36a7495dc2b516196e583
Paul Dickens <dickens.paul@gmail.com>
2019-12-01 17:47:04 +1100

    check dex endpoints

--- check-share.js
+++ check-share.js
@@ -1,7 +1,27 @@
 const storage = require('node-persist');
+const request = require('request');
+
+const shareUS = 'https://share1.dexcom.com'
+const shareOUS = 'https://shareous1.dexcom.com'
+
+// from https://github.com/nightscout/share2nightscout-bridge/issues/15
+const systemTime = '/ShareWebServices/Services/General/SystemUtcTime'
+
+request(`${shareOUS}${systemTime}`, function (error, response, body) {
+  console.log('errorOUS:', error); // Print the error if one occurred
+  console.log('statusCodeOUS:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('bodyOUS:', body); // Print the HTML for the Google homepage.
+});
+
+request(`${shareUS}${systemTime}`, function (error, response, body) {
+  console.log('errorUS:', error); // Print the error if one occurred
+  console.log('statusCodeUS:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('bodyUS:', body); // Print the HTML for the Google homepage.
+});
 
 (async () => {
-  await storage.init( );
-  await storage.setItem('us','2019-12-01T05:54:48.7887936Z0');
-  await storage.setItem('ous','2019-12-01T05:54:48.7887936Z0');
+  await storage.init();
+  const now = new Date().toISOString();
+  await storage.setItem('us', now);
+  await storage.setItem('ous', now);
 })();
