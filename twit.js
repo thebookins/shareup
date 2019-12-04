fdc8bb42325e00fa646ba63a607ccc5279435652
Paul Dickens <dickens.paul@gmail.com>
2019-12-05 09:44:59 +1100

    fix mistake

--- twit.js
+++ twit.js
@@ -3,10 +3,10 @@ const Twit = require('twit');
 const T = new Twit({
   consumer_key:         process.env.BOT_CONSUMER_KEY,
   consumer_secret:      process.env.BOT_CONSUMER_SECRET,
-  access_token:         access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token:         process.env.BOT_ACCESS_TOKEN,
   access_token_secret:  process.env.BOT_ACCESS_TOKEN_SECRET,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  strictSSL:            false,     // optional - requires SSL certificates to be valid.
 })
 
 // var phraseArray = [ "hey twitter",
